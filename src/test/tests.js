bbb8f5a0f2523eb5ef342d9741a3a2ad6cb5323d
Ben Foxall <benfoxall@gmail.com>
2014-08-01 18:52:29 +0100

    added tests for other circles

--- src/test/tests.js
+++ src/test/tests.js
@@ -1,14 +1,25 @@
 describe('circler', function(){
-	it('works for a basic circle', function(){
-		var c = circler([0,1],[1,0],[-1,0])
+	it('works for a circle', function(){
+		circler([0,1],[1,0],[-1,0])
+			.should.eql({
+				center:[0,0],
+				radius:1
+			})
+	})
 
-		c.center.should.eql([0,0])
-		c.radius.should.eql(1)
+	it('works for a circle with a different centre', function(){
+		circler([2,6],[3,5],[1,5])
+			.should.eql({
+				center:[2,5],
+				radius:1
+			})
+	})
+
+	it('works for a circle of a different radius', function(){
+		circler([0,2],[2,0],[-2,0])
+			.should.eql({
+				center:[0,0],
+				radius:2
+			})
 	})
 })
-
-function assert(expected){
-	if(expected){
-		throw new Error("assert failed")
-	}
-}
